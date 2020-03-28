80bf9d12de415a1504062007c61da6102cfca456
Alvaro Leal <pursuance@gmail.com>
2020-03-28 18:36:54 +0100

    Update form

--- packages/www/src/components/SenderForm/SenderForm.js
+++ packages/www/src/components/SenderForm/SenderForm.js
@@ -13,7 +13,7 @@ const SenderForm = () => (
           <form
             id="sender-subscribe"
             className="sender-subscribe"
-            action="https://app.sender.net/forms/sender_subscription/10172/c9547b90"
+            action="https://app.sender.net/forms/sender_subscription/10172/b9825313"
             method="POST"
           >
             <div
@@ -29,7 +29,9 @@ const SenderForm = () => (
                 required
               />
             </div>
-            <Button ml={3} className="submit-button" type="submit">Enviar</Button>
+            <Button ml={3} className="submit-button" type="submit">
+              Enviar
+            </Button>
           </form>
         </div>
         <a

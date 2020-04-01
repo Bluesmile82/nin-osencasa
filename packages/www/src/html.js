8d6bf6dc5ea3bb6ee27f9d1705088b39282034b2
Alvaro Leal <pursuance@gmail.com>
2020-04-01 22:33:56 +0200

    Add google adds

---
+++ packages/www/src/html.js
@@ -0,0 +1,45 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+export default function HTML(props) {
+  const ads = process.env.NODE_ENV === 'production' && (
+    <script
+      async
+      data-ad-client="ca-pub-8969296292467580"
+      src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+    />
+  );
+
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        {props.headComponents}
+        {ads && ads}
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <div
+          key={`body`}
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}
+      </body>
+    </html>
+  );
+}
+
+HTML.propTypes = {
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.array,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
+  body: PropTypes.string,
+  postBodyComponents: PropTypes.array,
+}
